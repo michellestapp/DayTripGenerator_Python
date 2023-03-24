99cbc2d92f359b99273286d1b77a34e6180822c7
michellestapp <mstapp@rocketmail.com>
2023-03-24 15:15:05 -0500

    Added random code calls

--- main.py
+++ main.py
@@ -4,16 +4,39 @@
 
 
 # (5 points): As a developer, I want to make at least three commits with descriptive messages 
-# (5 points):  As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment in their own separate Lists. 
-# (5 points): As a user, I want a destination to be randomly selected for my day trip. 
-# (5 points): As a user, I want a restaurant to be randomly selected for my day trip
-# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. 
-# (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
-# (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
+# DONE (5 points):  As a developer, I want to store my destinations, restaurants, mode of transportation, and 
+#              entertainment in their own separate Lists. 
+# DONE (5 points): As a user, I want a destination to be randomly selected for my day trip. 
+# DONE (5 points): As a user, I want a restaurant to be randomly selected for my day trip
+# DONE (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. 
+# DONE (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+# (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of 
+#             transportation, and/or form of entertainment if I don’t like one or more of those things.
+# (10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of 
+#             the random selections.
+# (10  points): As a user, I want to display my completed trip in the console
+# (5 points): Single Responsibility: As a developer, I want all of my functions to have a Single Responsibility. 
+#             Remember, each function should do just one thing! 
 
 
 destinations = ['Puerto Vallarta', 'Chicago', 'Albuquerque', 'New york City', 'Orlando']
 restaurants = ['Cracker Barrel', 'Olive Garden', 'IHOP', 'Golden Corral', 'Mi Lupita', 'Los Cuates', 'The Frontier']
 transportations = ['air','car','carriage','bus', 'limo', 'walking']
 entertainments = ['movie','bar','aquarium','amusment park', 'pool','luau']
+
+
+import random
+
+destination = random.choice(destinations)
+print(destination)
+restaurant = random.choice(restaurants)
+transportation = random.choice(transportations)
+entertainment = random.choice(entertainments)
+
+print(f"Your amazing day trip will go to {destination} by {transportation}. You will eat at {restaurant} and then go to the {entertainment}.")
+
+
+
+
+
 
