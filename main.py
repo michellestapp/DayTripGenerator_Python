6a44e9e916c31c507035a81818e087aca9312acc
michellestapp <mstapp@rocketmail.com>
2023-03-24 14:55:04 -0500

    Added list to main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 # Day Trip Generator in Python
 
-print('Hello World')
+# Main Stories
+
+
+# (5 points): As a developer, I want to make at least three commits with descriptive messages 
+# (5 points):  As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment in their own separate Lists. 
+# (5 points): As a user, I want a destination to be randomly selected for my day trip. 
+# (5 points): As a user, I want a restaurant to be randomly selected for my day trip
+# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. 
+# (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+# (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
+
+
+destinations = ['Puerto Vallarta', 'Chicago', 'Albuquerque', 'New york City', 'Orlando']
+restaurants = ['Cracker Barrel', 'Olive Garden', 'IHOP', 'Golden Corral', 'Mi Lupita', 'Los Cuates', 'The Frontier']
+transportations = ['air','car','carriage','bus', 'limo', 'walking']
+entertainments = ['movie','bar','aquarium','amusment park', 'pool','luau']
 
